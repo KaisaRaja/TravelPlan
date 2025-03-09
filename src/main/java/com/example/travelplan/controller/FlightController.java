b087fe3888d4cbca56e50514a21d1d4ac0b47217
Kaisa <raja.kaisa@gmail.com>
2025-03-09 12:58:26 +0200

    08.03 added CrossOrigin in flightcontroller

--- src/main/java/com/example/travelplan/controller/FlightController.java
+++ src/main/java/com/example/travelplan/controller/FlightController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/flights")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class FlightController {
     private final FlightService flightService;
 
